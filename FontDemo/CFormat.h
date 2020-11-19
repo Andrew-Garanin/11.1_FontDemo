12f9b930bed6325cb2bc9466b30fc41ae4f12736
Andrew <andrey-garanin-00@mail.ru>
2020-11-20 02:22:42 +0300

    Добавьте файлы проекта.

---
+++ FontDemo/CFormat.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+enum { JUSTIFY_LEFT, JUSTIFY_CENTER, JUSTIFY_RIGHT };
+enum { PITCH_VARIABLE, PITCH_FIXED };
+
+// Диалоговое окно CFormat
+
+class CFormat : public CDialog
+{
+	DECLARE_DYNAMIC(CFormat)
+protected:
+	RECT m_RectSample;
+public:
+	CFormat(CWnd* pParent = nullptr);   // стандартный конструктор
+	virtual ~CFormat();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+	DECLARE_MESSAGE_MAP()
+public:
+	BOOL m_Bold;
+	BOOL m_Italic;
+//	BOOL m_Underline;
+	BOOL m_Underline;
+//	int m_Justify;
+//	int m_Pitch;
+	int m_Spacing;
+	int m_Justify;
+	int m_Pitch;
+	CEdit m_SpacingEdit;
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg void OnBnClickedBold();
+	afx_msg void OnBnClickedItalic();
+	afx_msg void OnBnClickedUnderline();
+	afx_msg void OnBnClickedLeft();
+	afx_msg void OnBnClickedCenter();
+	afx_msg void OnBnClickedRight();
+	afx_msg void OnBnClickedVariable();
+	afx_msg void OnBnClickedFixed();
+	afx_msg void OnEnChangeSpacing();
+};
